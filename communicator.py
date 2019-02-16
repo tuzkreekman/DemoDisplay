5e3e6bf0f0001475fea9aeee3772442c0cebe1d7
Zainab Faryal Khan <zainabk@stanford.edu>
2019-02-15 16:04:22 -0800

    added comm unit to seperate out that module

---
+++ communicator.py
@@ -0,0 +1,10 @@
+
+def send_mnist_image(array):
+    print "sent image", array
+    return 3 #replace with response
+
+def send_svhn_index(index):
+    print "Sent index: %d" % index 
+    return 3 #replace with response
+
+
